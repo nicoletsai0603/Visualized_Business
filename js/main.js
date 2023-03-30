a239ed5c288cb73f19075a7568629638e47a04f5
nicole0603 <liz.coco.candy@gmail.com>
2023-03-30 17:55:09 +0800

    48484

---
+++ js/main.js
@@ -0,0 +1,160 @@
+$(function() {
+
+    $(".sub_btn1-1")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').removeClass('on');
+        })
+
+    $(".sub_btn1-2")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').removeClass('on');
+        })
+    $(".sub_btn1-3")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').removeClass('on');
+        })
+    $(".sub_btn2-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').removeClass('on');
+        })
+    $(".sub_btn2-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').removeClass('on');
+        })
+    $(".sub_btn2-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').removeClass('on');
+        })
+    $(".sub_btn3-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').removeClass('on');
+        })
+    $(".sub_btn3-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').removeClass('on');
+        })
+    $(".sub_btn3-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').removeClass('on');
+        })
+    $(".sub_btn4-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').removeClass('on');
+        })
+    $(".sub_btn4-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').removeClass('on');
+        })
+    $(".sub_btn4-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').removeClass('on');
+        })
+})
