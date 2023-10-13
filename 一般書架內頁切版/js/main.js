ae4ac6bca30b0d70130b6cedcbfe51f7ac8387cc
nicole0603 <liz.coco.candy@gmail.com>
2023-10-13 18:30:13 +0800

    8484

---
+++ 一般書架內頁切版/js/main.js
@@ -0,0 +1,255 @@
+$(function() {
+
+    $(".sub_btn1-1,.pc1-1")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').removeClass('on');
+        })
+
+    $(".sub_btn1-2,.pc1-2")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').removeClass('on');
+        })
+    $(".sub_btn1-3,.pc1-3")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').removeClass('on');
+        })
+    $(".sub_btn2-1,.pc2-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').removeClass('on');
+        })
+    $(".sub_btn2-2,.pc2-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').removeClass('on');
+        })
+    $(".sub_btn2-3,.pc2-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').removeClass('on');
+        })
+    $(".sub_btn3-1,.pc3-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').removeClass('on');
+        })
+    $(".sub_btn3-2,.pc3-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').removeClass('on');
+        })
+    $(".sub_btn3-3,.pc3-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').removeClass('on');
+        })
+    $(".sub_btn4-1,.pc4-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').removeClass('on');
+        })
+    $(".sub_btn4-2,.pc4-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').removeClass('on');
+        })
+    $(".sub_btn4-3,.pc4-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').removeClass('on');
+        })
+})
+
+
+
+
+
+var swiper1 = new Swiper("#mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 1,
+    navigation: {
+        nextEl: ".swiper-button-next-1",
+        prevEl: ".swiper-button-prev-1",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        1111: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        1250: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        1300: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+    },
+});
+var swiper2 = new Swiper("#mySwiper2", {
+    slidesPerView: 1,
+    spaceBetween: 1,
+
+    navigation: {
+        nextEl: ".swiper-button-next-2",
+        prevEl: ".swiper-button-prev-2",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        1111: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        1250: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        1300: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+    },
+});
+
+var swiper3 = new Swiper("#mySwiper3", {
+    slidesPerView: 1,
+    spaceBetween: 1,
+    navigation: {
+        nextEl: ".swiper-button-next-3",
+        prevEl: ".swiper-button-prev-3",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 5,
+        },
+        1111: {
+            slidesPerView: 2,
+            spaceBetween: 5,
+        },
+        1250: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        1300: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+    },
+});
+
+
+$(".Click_menu a").on("click", function() {
+    var hrefLink = $(this).attr("name");
+    $("html,body").animate({
+            scrollTop: $(hrefLink).offset().top //錨點
+        },
+        1000
+    );
+});
---
+++ 不規則書架內頁切版/js/main.js
@@ -0,0 +1,188 @@
+$(function() {
+
+    $(".sub_btn1-1,.pc1-1")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-1-yellow').removeClass('on');
+        })
+
+    $(".sub_btn1-2,.pc1-2")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-2-yellow').removeClass('on');
+        })
+    $(".sub_btn1-3,.pc1-3")
+        .mouseover(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc2bg,.pc3bg,.pc4bg,.gbtnbg,#pc1-3-yellow').removeClass('on');
+        })
+    $(".sub_btn2-1,.pc2-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-1-yellow').removeClass('on');
+        })
+    $(".sub_btn2-2,.pc2-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-2-yellow').removeClass('on');
+        })
+    $(".sub_btn2-3,.pc2-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc3bg,.pc4bg,.gbtnbg,#pc2-3-yellow').removeClass('on');
+        })
+    $(".sub_btn3-1,.pc3-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-1-yellow').removeClass('on');
+        })
+    $(".sub_btn3-2,.pc3-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-2-yellow').removeClass('on');
+        })
+    $(".sub_btn3-3,.pc3-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc4bg,.gbtnbg,#pc3-3-yellow').removeClass('on');
+        })
+    $(".sub_btn4-1,.pc4-1")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-1-yellow').removeClass('on');
+        })
+    $(".sub_btn4-2,.pc4-2")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-2-yellow').removeClass('on');
+        })
+    $(".sub_btn4-3,.pc4-3")
+        .mouseover(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').addClass('on');
+        })
+        .mouseout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').removeClass('on');
+        })
+        .focus(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').addClass('on');
+        })
+        .focusout(function() {
+            $('.pc1bg,.pc2bg,.pc3bg,.gbtnbg,#pc4-3-yellow').removeClass('on');
+        })
+})
+
+
+
+
+
+var swiper1 = new Swiper("#mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 1,
+    navigation: {
+        nextEl: ".swiper-button-next-1",
+        prevEl: ".swiper-button-prev-1",
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+        renderBullet: function(index, className) {
+            return '<span class="' + className + '">' + (index + 1) + '</span>';
+        },
+    },
+    breakpoints: {
+
+        1300: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+    },
+});
